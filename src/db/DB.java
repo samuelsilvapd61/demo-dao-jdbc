f9f6013d67584bc4232c6dde055304303e9b0a2a
Samuel Silva <samuelsilvapd61@gmail.com>
2021-12-23 23:46:08 -0300

    Final version

--- src/db/DB.java
+++ src/db/DB.java
@@ -10,7 +10,7 @@ import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
-
+	
 	private static Connection conn = null;
 	
 	public static Connection getConnection() {
@@ -19,32 +19,35 @@ public class DB {
 				Properties props = loadProperties();
 				String url = props.getProperty("dburl");
 				conn = DriverManager.getConnection(url, props);
-			}
-			catch (SQLException e) {
+				
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
+			
 		}
 		return conn;
+	}
+	
+	private static Properties loadProperties() {
+		
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+			
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
 	}
 	
 	public static void closeConnection() {
 		if (conn != null) {
 			try {
 				conn.close();
+				
 			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
-		}
-	}
-	
-	private static Properties loadProperties() {
-		try (FileInputStream fs = new FileInputStream("db.properties")) {
-			Properties props = new Properties();
-			props.load(fs);
-			return props;
-		}
-		catch (IOException e) {
-			throw new DbException(e.getMessage());
 		}
 	}
 	
@@ -52,19 +55,22 @@ public class DB {
 		if (st != null) {
 			try {
 				st.close();
+				
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+				
 			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
 
-	public static void closeResultSet(ResultSet rs) {
-		if (rs != null) {
-			try {
-				rs.close();
-			} catch (SQLException e) {
-				throw new DbException(e.getMessage());
-			}
-		}
-	}
 }
--- src/db/DbException.java
+++ src/db/DbException.java
@@ -1,9 +1,11 @@
 package db;
 
-public class DbException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
+public class DbException extends RuntimeException{
 
+	private static final long serialVersionUID = 1L;
+	
 	public DbException(String msg) {
 		super(msg);
 	}
+
 }
